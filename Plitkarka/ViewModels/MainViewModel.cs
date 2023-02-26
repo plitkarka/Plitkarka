08e8f6f03b1dcb5663b73041ac401ce150fd2e71
Anastasiia Lusta <75475914+AnastasiaLusta@users.noreply.github.com>
2023-02-26 21:52:06 +0200

    feature: add viewmodel for main page

---
+++ Plitkarka/ViewModels/MainViewModel.cs
@@ -0,0 +1,27 @@
+using System.Reactive;
+using Plitkarka.Views;
+using ReactiveUI;
+
+namespace Plitkarka.ViewModels;
+
+public class MainViewModel : ReactiveObject
+{
+    private readonly INavigation _navigation;
+    public ReactiveCommand<Unit, Task> OpenRegistrationFormCommand { get; }
+    public ReactiveCommand<Unit, Task> OpenLoginFormCommand { get; }
+    
+    public MainViewModel(INavigation navigation)
+    {
+        _navigation = navigation;
+        
+        OpenRegistrationFormCommand = ReactiveCommand.Create(async () =>
+        {
+            await _navigation.PushAsync(new RegistrationPage());
+        });
+        
+        OpenLoginFormCommand = ReactiveCommand.Create(async () =>
+        {
+            await _navigation.PushAsync(new LoginPage());
+        });
+    }
+}
